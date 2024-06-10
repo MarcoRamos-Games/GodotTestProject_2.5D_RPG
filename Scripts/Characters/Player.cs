0edd0a7f67101a6847a1fadb615753a1c09a6d39
Marco Ramos <marcoramosg928@gmail.com>
2024-06-09 19:41:54 -0700

    Adde player movement with inputs(wasd)

--- Scripts/Characters/Player.cs
+++ Scripts/Characters/Player.cs
@@ -3,13 +3,19 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    private Vector2 direction = new();
     public override void _PhysicsProcess(double delta)
     {
-        //GD.Print("Player physics process");
+        Velocity = new(direction.X, 0, direction.Y);
+        Velocity *= 5;
+
+        MoveAndSlide();
     }
 
     public override void _Input(InputEvent @event)
     {
-        GD.Print("Player detect input");
+        direction = Input.GetVector(      
+            "MoveLeft", "MoveRight", "MoveForward", "MoveBackward"
+        );
     }
 }
