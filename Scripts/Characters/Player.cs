6f9954fef24f6ccd4fbda68f47d4c6cee30a1a27
Marco Ramos <marcoramosg928@gmail.com>
2024-06-13 18:02:44 -0700

    Adde Animations(Idle, Move) and created GameConstants class to manage Strings

--- Scripts/Characters/Player.cs
+++ Scripts/Characters/Player.cs
@@ -1,9 +1,18 @@
 using Godot;
 using System;
+using System.Reflection.Metadata;
 
 public partial class Player : CharacterBody3D
 {
+    [ExportGroup("Required Nodes")]
+    [Export] private AnimationPlayer animPlayerNode;
+    [Export] private Sprite3D sprite3DNode;
     private Vector2 direction = new();
+
+    public override void _Ready()
+    {
+        animPlayerNode.Play(GameCosntants.ANIM_IDLE);
+    }
     public override void _PhysicsProcess(double delta)
     {
         Velocity = new(direction.X, 0, direction.Y);
@@ -15,7 +24,15 @@ public partial class Player : CharacterBody3D
     public override void _Input(InputEvent @event)
     {
         direction = Input.GetVector(      
-            "MoveLeft", "MoveRight", "MoveForward", "MoveBackward"
+            GameCosntants.INPUT_MOVE_LEFT, GameCosntants.INPUT_MOVE_RIGHT, GameCosntants.INPUT_MOVE_FORWARD, GameCosntants.INPUT_MOVE_BACKWARD
         );
+        if(direction == Vector2.Zero)
+        {
+            animPlayerNode.Play(GameCosntants.ANIM_IDLE);
+        }
+        else
+        {
+            animPlayerNode.Play(GameCosntants.ANIM_Move);
+        }
     }
 }
---
+++ Scripts/General/GameConstants.cs
@@ -0,0 +1,15 @@
+public class GameCosntants
+{
+    #region Animation Constants
+    public const string ANIM_IDLE = "Idle";
+    public const string ANIM_Move = "Move";
+    #endregion
+
+    #region  Input Constants
+    public const string INPUT_MOVE_LEFT = "MoveLeft";
+    public const string INPUT_MOVE_RIGHT = "MoveRight";
+    public const string INPUT_MOVE_FORWARD = "MoveForward";
+    public const string INPUT_MOVE_BACKWARD = "MoveBackward";
+    #endregion
+
+}
